9dcac489febd0db656d3123268226b8aff027b91
Prahlad Pallav <135969574+prahlad-pallav@users.noreply.github.com>
2024-01-03 14:05:17 +0530

    Create Chapter2_videos.cpp

---
+++ Chapter2_videos.cpp
@@ -0,0 +1,28 @@
+// importing header files
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;	
+
+int main() {
+
+  // path of the video
+	string path = "Resources/test_video.mp4";
+	
+  // videoCapture object
+  VideoCapture cap(path);
+
+	Mat img;
+
+	while (true) {
+		cap.read(img);
+		imshow("Image", img);
+		// add delay of 20 milliseconds
+    waitKey(20);
+	}
+
+	return 0;
+}
