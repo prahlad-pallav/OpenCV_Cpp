64cc54af1cc73e02913595b6b7ae37c05678cc96
Prahlad Pallav <135969574+prahlad-pallav@users.noreply.github.com>
2024-01-03 13:56:03 +0530

    Update Chapter1_images.cpp

--- Chapter1_images.cpp
+++ Chapter1_images.cpp
@@ -1,17 +1,24 @@
+// importing header files
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
 
 using namespace cv;
-using namespace std;
+using namespace std;	
 
 int main() {
 
-	string path = "peacock-8440548_1280.jpg";
+	// path of the image
+	string path = "Resources/lambo.png";
+
+	// Mat -> matrix datatype introduced by opencv to handle images
 	Mat img = imread(path);
 
+	// imshow -> to show our image
 	imshow("Image", img);
+
+	// add delay, 0 for infinite
 	waitKey(0);
 	return 0;
 }
