ce8a6c78ac1711e2eeffa70e1e6a6f15ebb82d95
Prahlad Pallav <135969574+prahlad-pallav@users.noreply.github.com>
2024-01-04 17:28:17 +0530

    Create Chapter19_colorPicker.cpp

---
+++ Chapter19_colorPicker.cpp
@@ -0,0 +1,48 @@
+// importing header files
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+
+int main() {
+
+	VideoCapture cap(0);
+	Mat img;
+
+	Mat imgHSV, mask;
+
+	int hmin = 0, smin = 110, vmin = 153;
+	int hmax = 19, smax = 240, vmax = 255;
+
+	namedWindow("Trackbars", (640, 200));
+
+	createTrackbar("Hue Min", "Trackbars", &hmin, 179);
+	createTrackbar("Hue Max", "Trackbars", &hmax, 179);
+	createTrackbar("Sat Min", "Trackbars", &smin, 255);
+	createTrackbar("Sat Max", "Trackbars", &smax, 255);
+	createTrackbar("Val Min", "Trackbars", &vmin, 255);
+	createTrackbar("Val Max", "Trackbars", &vmax, 255);
+
+	while (true) {
+
+		cap.read(img);
+		cvtColor(img, imgHSV, COLOR_BGR2HSV);
+
+		Scalar lower(hmin, smin, vmin);
+		Scalar upper(hmax, smax, vmax);
+
+		//CV_EXPORTS_W void inRange(InputArray src, InputArray lowerb, InputArray upperb, OutputArray dst);
+		inRange(imgHSV, lower, upper, mask);
+
+		imshow("Image", img);
+		imshow("HSV Image", imgHSV);
+		imshow("Color Picker", mask);
+		waitKey(1);
+	}
+
+	return 0;
+}
